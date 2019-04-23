88087661271dc948fcc682b47730f4a695571a59
Kabaye <svkulich@gmail.com>
2019-04-23 14:26:23 +0300

    fix: v2.4.2

--- src/main/java/by/bsu/kulich/Starter.java
+++ src/main/java/by/bsu/kulich/Starter.java
@@ -5,6 +5,6 @@ import main.java.by.bsu.kulich.game.Arkanoid;
 public class Starter {
 
     public static void main(String[] args) {
-        Arkanoid arkanoid = new Arkanoid();
+        new Arkanoid();
     }
 }
